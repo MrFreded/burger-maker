24a2053e34aaf71cd58dcc2b6a4eb0e628e15818
Fredrick Okoro <fredrick.okoro@rub.de>
2019-10-21 01:44:37 +0200

    modified package.json

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
@@ -9,6 +13,14 @@ const authRouter = require('./routes/authRouter');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(compression());
@@ -17,6 +29,11 @@ app.options('*', cors());
 
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 app.use('/api/v1', authRouter);
 app.use('/api/v1/orders', orderRouter);
 app.use('/api/v1/ingredients', ingredientsRouter);
