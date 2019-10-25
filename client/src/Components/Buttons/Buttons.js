06db342a3a92926d7aa90c882fdcf004f87a75f4
Fredrick Okoro <fredrick.okoro@rub.de>
2019-10-25 10:52:51 +0200

    buttom margins

--- client/src/Components/Buttons/Buttons.js
+++ client/src/Components/Buttons/Buttons.js
@@ -8,15 +8,15 @@ const Buttons = props => {
   return (
     <Col xs={12} className={styles.btnHolder}>
       <Row className={'justify-content-between'}>
-        <Col xs={4} className="text-right">
+        <Col xs={3} md={4} className="text-right">
           <label className={styles.label}>{props.ingredientType}</label>
         </Col>
-        <Col xs={4} className="text-center">
+        <Col xs={3} md={4} className="text-center">
           <Button className={styles.btn} clicked={props.MoreBtn}>
             More
           </Button>
         </Col>
-        <Col xs={4} className="text-left">
+        <Col xs={3} md={4} className="text-left">
           <Button className={styles.btn} clicked={props.LessBtn} disabled={props.lessBtnDisabled}>
             Less
           </Button>
